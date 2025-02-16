f87ff445ecab36573dec33d11da7d85a35593511
Asmit tyagi <ashmittyagigms@gmail.com>
2025-02-16 07:37:53 +0530

    Update environment configuration, metadata, and Navbar layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,15 @@
 import { ReactNode } from 'react';
 
 import StreamVideoProvider from '@/providers/StreamClientProvider';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "BharatBaithak",
+  description: "A Video Calling App",
+  icons: {
+    icon: "/icons/favicon.svg",
+  }
+};
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,16 +10,20 @@ const Navbar = async () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
-        <Image
-          src="/icons/logo.svg"
-          width={32}
-          height={32}
-          alt="bharatbaithak logo"
-          className="max-sm:size-10"
-        />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+        <div className="relative w-[200px] h-[50px] flex items-center justify-center">
+          <Image
+            src="/icons/logo.svg"
+            layout="intrinsic"
+            width={300}
+            height={60}
+            objectFit="contain"
+            alt="bharatbaithak logo"
+            className="bg-transparent"
+          />
+        </div>
+        {/* <p className="text-[26px] font-extrabold text-white max-sm:hidden">
           BharatBaithak
-        </p>
+        </p> */}
       </Link>
       {user && <div className="text-white">{`Welcome ${user?.firstName}`}</div>}{" "}
       <div className="flex-between gap-5">
