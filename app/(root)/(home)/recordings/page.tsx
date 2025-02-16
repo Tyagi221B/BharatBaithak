214fc35d51c98edd178482b35d97f3dce41e9bda
Asmit tyagi <ashmittyagigms@gmail.com>
2025-02-16 08:08:26 +0530

    Add CallList component to PreviousPage and RecordingPage

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import CallList from "@/components/CallList";
 
 const RecordingPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">RecordingPage</h1>
+
+      <CallList type="recordings" />
     </section>
   );
 };
