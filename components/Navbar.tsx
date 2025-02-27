211ed6ce8da3af39918d725732ea9d60385a9e50
Asmit tyagi <ashmittyagigms@gmail.com>
2025-02-27 12:45:28 +0530

    Remove unused user fetching logic from Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,8 @@ import Link from "next/link";
 
 import MobileNav from "./MobileNav";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
 
 const Navbar = async () => {
-  const user = await currentUser();
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
